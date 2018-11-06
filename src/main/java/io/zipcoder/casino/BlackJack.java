71948de1d089a34d8bbf519f8a6cfc8b345ef52d
ThuyKhong <thuykhong@zipcoders-MacBook-Pro-4.local>
2018-11-06 13:07:22 -0500

    hi thuy

--- src/main/java/io/zipcoder/casino/BlackJack.java
+++ src/main/java/io/zipcoder/casino/BlackJack.java
@@ -42,12 +42,10 @@ public class BlackJack extends CardGame implements Gamble {
     public int getValue(ArrayList<CardHand> cardHands){
         int value = 0;
         boolean ace = false;
-        int cards= cardHands.size();
 
-        for (int i = 0; i < cards; i++) {
+        for (int i = 0; i < cardHands.size(); i++) {
             int cardValue;
-            i = getCard(cardHands,i);
-            cardValue = getCardValue(i);
+            cardValue = cardHands.get(i);
 
             if (cardValue > 10)
             {
@@ -67,15 +65,6 @@ public class BlackJack extends CardGame implements Gamble {
 
     }
 
-    public int getCardValue(int numberOfCards){
-        int result = Card;
-        switch(Card){
-            case 11: result = 10;
-            case 12: result = 10;
-            case 13: result = 10;
-        }
-        return result;
-    }
 
     public int getCard(Vector cardHands, int position) {
         if (position >= 0 && position < cardHands.size())
