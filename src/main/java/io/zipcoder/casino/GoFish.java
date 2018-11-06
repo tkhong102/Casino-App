e685ed5169b04cec116be5ca5013e0a31566cc93
Jacqueline Joson <jacquelinejoson@zipcoders-MacBook-Pro-32.local>
2018-11-06 12:06:20 -0500

    {quick initial commit}

--- src/main/java/io/zipcoder/casino/GoFish.java
+++ src/main/java/io/zipcoder/casino/GoFish.java
@@ -17,14 +17,24 @@ public class GoFish extends CardGame {
     int setsCount;
 
     @Override
-    void dealCards(Player player, int numberOfCards) {
-        super.dealCards(player, numberOfCards);
-        //For 2 to 3 players you deal each player 7 cards.
-        //If there are more than three players, deal 5 cards each.
+    void dealCards(Player player, int numberOfCards) {              //not sure if we should be taking in an int
+        super.dealCards(player, numberOfCards);                     // of cards as a param. Maybe save that for
+            if (goFishPlayers.size() <= 3) {                        // if/else statement to determine #ofcards??
+                numberOfCards = 7;                                  // or initialize numberOfCards as 0 before?
+            } else {
+                numberOfCards = 5;
+            }
+        //RULES: For 3 players or less, deal each player 7 cards; If more than three players, deal 5 cards each.
     }
 
+    //while (Deck.cards(?) > 0) {
     public void questionPlayer(String cardRank, Player player) {
-        //player surrenders cards of specified rank
+        //start w player
+        //askFishQuestion(); Prompt w/ question of who to ask [console output?]
+        //take userInput [console input?]
+        //Prompt w/question of what card (rank) to look for [console output?]
+        //take userInput[console input?]
+
     }
 
     public void evalCards() {
