9da0d33d5d51af49ebe08c910da47810334eea7b
Connor <cgdunni14@gmail.com>
2018-11-07 15:55:53 -0500

    updated craps

--- src/main/java/io/zipcoder/casino/dicegames/Craps.java
+++ src/main/java/io/zipcoder/casino/dicegames/Craps.java
@@ -15,7 +15,6 @@ public class Craps extends DiceGame implements Gamble {
 
     public Craps() {
         readyPlayers();
-        placeBet();
         run();
     }
     //for test purposes
@@ -32,35 +31,52 @@ public class Craps extends DiceGame implements Gamble {
 
     public void run(){
         for(int i = 0; i < crapsPlayers.size(); i++){
-            play(crapsPlayers.get(i),crapsPlayers.get(i).getBet());
+            greetPlayer(crapsPlayers.get(i).getP());
+            play(crapsPlayers.get(i));
         }
     }
 
     public void placeBet() {
-        bet = console.getIntegerInput("Enter your bet");
+        this.bet = console.getIntegerInput("Enter your bet");
     }
 
     public void evaluateBet(Player player, long payout) {
         player.setChipBalance(player.getChipBalance()+payout);
     }
 
-    public void play(CrapsPlayer currentPlayer, long bet) {
-
-        this.bet = bet;
-
+    public void play(CrapsPlayer currentPlayer) {
+        placeBet();
+        promptEnterKey("roll dice");
         int sum = rollDie(2); // roll two dice, store sum in sum field.
-
+        console.println("Your roll sum equals: " + sum);
         if (sum == 7 || sum == 11) {
+            console.println("\n*********");
+            console.println("YOU WIN!");
+            console.println("*********\n");
             evaluateBet(currentPlayer.getP(), bet*2);
         } else if (sum == 2 || sum == 3 || sum == 12) {
+            console.println("\n*********");
+            console.println("YOU LOSE!");
+            console.println("*********\n");
             evaluateBet(currentPlayer.getP(), -(bet*2));
         } else {
             int point = sum;
             do {
+                console.println("\n--------------------");
+                console.println("Point to roll for: " + point);
+                console.println("--------------------");
+                promptEnterKey("roll again");
                 sum = rollDie(2);
+                console.println("You rolled a " + sum);
                 if (sum == 7) {
+                    console.println("\n*********");
+                    console.println("YOU LOSE!");
+                    console.println("*********\n");
                     evaluateBet(currentPlayer.getP(), -bet);
                 } else if (sum == point) {
+                    console.println("\n*********");
+                    console.println("YOU WIN!");
+                    console.println("*********\n");
                     evaluateBet(currentPlayer.getP(), bet);
                 }
             } while (sum != point && sum != 7);
@@ -71,5 +87,13 @@ public class Craps extends DiceGame implements Gamble {
         for(int i = 0; i < crapsPlayers.size(); i++){
             System.out.println(crapsPlayers.get(i).getP().getChipBalance());
         }
+    }
+
+    public void greetPlayer(Player playa){
+        console.println("Ok " + playa.getName() + ", you're up...\n");
+    }
+
+    public void promptEnterKey(String str){
+        String input = console.getStringInput("\nPress \"ENTER\" to " + str);
     }
 }
